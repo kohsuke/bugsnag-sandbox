6b1936c252aed65ebab120ea73e2c5ee2014d115
Kohsuke Kawaguchi <kk@kohsuke.org>
2018-03-27 11:53:58 -0700

    Initial version

---
+++ src/main/java/test/App.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.bugsnag.Bugsnag;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        foo();
+    }
+
+    private static void foo() {
+        Bugsnag bugsnag = new Bugsnag(System.getenv("KEY"));
+        bugsnag.setAppVersion("1.0.0");
+        bugsnag.addCallback((report)-> {
+            report.addToTab("sample tab","key1","value2");
+        });
+        bugsnag.notify(new Exception("Yo!"));
+    }
+}
